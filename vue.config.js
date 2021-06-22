b9952d8b7e5e9b1434e4d4efe320fa4d4317641a
oguni <orkney092@gmail.com>
2021-06-22 15:34:20 +0900

    リポジトリ名の修正

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath: isProd ? '/リポジトリ名' : '/',
+  publicPath: isProd ? '/gas_household' : '/',
   outputDir: 'docs',
   filenameHashing: false,
   productionSourceMap: false,
