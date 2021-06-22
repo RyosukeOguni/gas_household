4cff33461cd184ac67105fdfcb7ac08672351e75
oguni <orkney092@gmail.com>
2021-06-22 15:26:41 +0900

    production用の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   transpileDependencies: ['vuetify'],
+  publicPath: isProd ? '/リポジトリ名' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false,
 }
